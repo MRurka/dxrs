91a2995d2a228a037e1275f008aaa226f292310b
MRurka <michael@rurkadesign.com>
2018-11-06 13:32:01 +0900

    .

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,7 +2,8 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-
+  "component---cache-dev-404-page-js": () => import("/Users/Rurka/dxrs/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-index-js": () => import("/Users/Rurka/dxrs/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
 }
 
 exports.data = () => import("/Users/Rurka/dxrs/.cache/data.json")
