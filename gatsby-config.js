7ae1a3e5032cbe9353a09a0b89f5919dc4805177
MRurka <michael@rurkadesign.com>
2018-11-06 14:25:40 +0900

    Actual working state. Don't double up module.exports

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,5 @@
 module.exports = {
   plugins: [`gatsby-plugin-netlify-cms`],
-}
-
-module.exports = {
   siteMetadata: {
     title: "My Homepage",
     description: "This is where I write my thoughts.",
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,7 @@
 import React from "react"
-import { graphql } from 'gatsby'
 
 const HomePage = () => {
-  return (
-    <div>
-      Hello!
-    </div>
-  )
+  return <div>Hello!</div>
 }
-
-// export const query = graphql`
-//   # query will go here
-// `
 
 export default HomePage
