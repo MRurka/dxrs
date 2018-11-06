d164622bf5a6c0f656f5a014f2d1d627a9961b4f
MRurka <michael@rurkadesign.com>
2018-11-06 15:51:55 +0900

    super works.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,14 @@
 module.exports = {
-  plugins: [`gatsby-plugin-netlify-cms`],
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/blog/`,
+        name: "markdown-pages",
+      },
+    },
+  ],
   siteMetadata: {
     title: "My Homepage",
     description: "This is where I write my thoughts.",
