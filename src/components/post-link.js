d5db72add057a7bd3d7e711db18198fe4d5e2c9f
MRurka <michael@rurkadesign.com>
2018-11-21 16:03:19 +0900

    Woo. it works :D

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 import "./post-link.scss"
 
 const PostLink = ({ post }) => (
-  <div className="post-link" backgroundImage={post.frontmatter.image} >
+  <div className="post-link" style={{backgroundImage: "url(" + post.frontmatter.image + ")" }}>
     <Link to={post.frontmatter.path}>
       {post.frontmatter.title} ({post.frontmatter.date})
     </Link>
