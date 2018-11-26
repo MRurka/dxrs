ce9035d2a6c1b2b8d8dfb865e13a232a1fe35494
MRurka <michael@rurkadesign.com>
2018-11-26 13:44:52 +0900

    Add proper title/subtitle in CMS and post.

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -1,19 +1,25 @@
 import React from "react"
 import { Link } from "gatsby"
 import "./post-link.scss"
-import ButtonArrow from "../components/button-arrow.js"
+import IconArrow from "../components/icon-arrow.js"
 
 const PostLink = ({ post }) => (
 
-  <div className="post-link" style={{backgroundImage: "url(" + post.frontmatter.thumbnail + ")" }}>
-    <h1>{post.frontmatter.title}</h1>
-    <Link to={post.frontmatter.path}>
-      <ButtonArrow></ButtonArrow>
+  <div className="post" style={{backgroundImage: "url(" + post.frontmatter.thumbnail + ")" }}>
+    <h1>
+      {post.frontmatter.first}
+      <span>{post.frontmatter.nickname}</span>
+      {post.frontmatter.last}
+    </h1>
+    <div>
+      {post.frontmatter.description}
+    </div>
+    <Link className="post-link" to={post.frontmatter.path}>
+      <IconArrow></IconArrow>
     </Link>
   </div>
 )
 
 export default PostLink
-
 
 // ({post.frontmatter.date})
