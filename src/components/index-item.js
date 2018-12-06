8e8b2c0b04ec4188d1d122c2f574ec509a9d4901
MRurka <michael@rurkadesign.com>
2018-12-06 14:18:56 -0500

    add gradient backdrop

--- src/components/index-item.js
+++ src/components/index-item.js
@@ -6,19 +6,23 @@ import IconArrow from "../components/icon-arrow.js"
 const PostLink = ({ post }) => (
 
   <div className="index-item" style={{backgroundImage: "url(" + post.frontmatter.thumbnail + ")" }}>
-    <div>
-      <h1 className="index-item-title">
-        <span>{post.frontmatter.first}</span>
-        <span className="accent">{post.frontmatter.nickname}</span>
-        <span>{post.frontmatter.last}</span>
-      </h1>
-      <div className="index-item-subtitle">
-        {post.frontmatter.subtitle}
-      </div>
+    <div className="index-item-backdrop-gradient">
     </div>
-    <Link className="index-item-link" to={post.frontmatter.path}>
-      <IconArrow></IconArrow>
-    </Link>
+    <div className="index-item-content">
+      <div>
+        <h1 className="index-item-title">
+          <span>{post.frontmatter.first}</span>
+          <span className="accent">{post.frontmatter.nickname}</span>
+          <span>{post.frontmatter.last}</span>
+        </h1>
+        <div className="index-item-subtitle">
+          {post.frontmatter.subtitle}
+        </div>
+      </div>
+      <Link className="index-item-link" to={post.frontmatter.path}>
+        <IconArrow></IconArrow>
+      </Link>
+    </div>
   </div>
 )
 
