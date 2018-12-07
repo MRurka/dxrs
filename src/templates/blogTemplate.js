bed6ce1c8b477ebf2d9fd6e908bff401b3bac730
MRurka <michael@rurkadesign.com>
2018-12-07 13:09:59 -0500

    Unfinished blog page template styling.

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -10,13 +10,16 @@ export default function Template({
   const { frontmatter, html } = markdownRemark
   return (
     <div className="blog-post">
-      <div className="blog-post-header" style={{backgroundImage: "url(" + frontmatter.thumbnail + ")" }}>
+      <div className="blog-post-header" style={{backgroundImage: "url(" + frontmatter.banner+ ")" }}>
         <div className="container">
           <h1 className="index-item-title">
             <span>{frontmatter.first}</span>
             <span className="accent">{frontmatter.nickname}</span>
             <span>{frontmatter.last}</span>
           </h1>
+          <div className="index-item-date">
+            {frontmatter.date}
+          </div>
           <div className="index-item-subtitle">
             {frontmatter.subtitle}
           </div>
@@ -39,13 +42,13 @@ export const pageQuery = graphql`
     markdownRemark(frontmatter: { path: { eq: $path } }) {
       html
       frontmatter {
-        date(formatString: "MMMM DD, YYYY")
+        date(formatString: "MM DD YY")
         subtitle
         path
         first
         nickname
         last
-        thumbnail
+        banner
       }
     }
   }
