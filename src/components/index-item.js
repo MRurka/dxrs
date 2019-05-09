8eba2f399dd3cc29b56a1905708baac9a0955f8d
MRurka <michael@rurkadesign.com>
2019-05-09 06:59:44 -0400

    .

--- src/components/index-item.js
+++ src/components/index-item.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
 import styles from "./index-item.module.scss"
-import IconArrow from "../components/icon-arrow.js"
 
 const PostLink = ({ post }) => (
 
