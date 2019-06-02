5e142ee4ef58b3861abf86815971a120c71f5c9a
MRurka <michael@rurkadesign.com>
2019-06-02 17:49:16 -0400

    Update: About page copy.

--- src/pages/about.js
+++ src/pages/about.js
@@ -20,8 +20,8 @@ export default AboutPage => (
         </div>
         <div className={styles.markdownContent}>
           <p>
-            WOLO is a global community that believes in the pursuit of purpose, experiences and the betterment of oneself and the people around us, as the foundation to a meaningful life.
-            Together, we sustain positive momentum, support, counsel and lift each other when needed.
+            WOLO is a global community that believes in the pursuit of purpose, experiences and the betterment of oneself and the people around us. This is the way to a life of meaning.
+            Together we sustain positive momentum via mutual support, counsel and encouragement.
           </p>
           <p>
             This belief manifests itself in the following ways:
@@ -30,7 +30,7 @@ export default AboutPage => (
             <li>Private events and retreats focused on wellness and personal development</li>
             <li>Regular goals and accountability meetups</li>
             <li>Sharing of resources and cross pollination of networks</li>
-            <li>Counsel meetings to help think through difficult decisions</li>
+            <li>Counsel to help work through various deliberations</li>
           </ul>
         </div>
       </div>
